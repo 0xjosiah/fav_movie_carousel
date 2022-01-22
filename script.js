f15980a22f9ac41dad10bc867c933229b291c472
josiahwebb <jj.webb520@gmail.com>
2022-01-22 00:10:42 -0700

    basic carousel works

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 const nextBtn = document.getElementById('next')
 const prevBtn = document.getElementById('previous')
-const movies = document.getElementById('movies')
+const movies = document.getElementById('movies').children
 
 console.log(movies)
 
 
+let count = 0
 nextBtn.addEventListener('click', function() {
-    
+    if (count < 7) {
+        count += 1
+        movies[count-1].classList.remove('carousel-item-visible')
+        movies[count].classList.add('carousel-item-visible')
+    } else {
+        count -= 7
+        movies[count+7].classList.remove('carousel-item-visible')
+        movies[count].classList.add('carousel-item-visible')
+    }
+})
+
+prevBtn.addEventListener('click', function() {
+    if (count > 0 && count < 8) {
+        count -= 1
+        movies[count+1].classList.remove('carousel-item-visible')
+        movies[count].classList.add('carousel-item-visible')
+    } else {
+        count += 7
+        movies[count].classList.add('carousel-item-visible')
+        movies[count-7].classList.remove('carousel-item-visible')
+    } 
 })
