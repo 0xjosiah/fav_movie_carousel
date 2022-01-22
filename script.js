adcc6ecaa7204fa6764e99fefa86db6b672b2e3d
josiahwebb <jj.webb520@gmail.com>
2022-01-21 17:50:46 -0700

    basic, rough scaffold for movie carousel site

---
+++ script.js
@@ -0,0 +1,10 @@
+const nextBtn = document.getElementById('next')
+const prevBtn = document.getElementById('previous')
+const movies = document.getElementById('movies')
+
+console.log(movies)
+
+
+nextBtn.addEventListener('click', function() {
+    
+})
